ce7bfc449c7463157eb0df9f79b90acc3501ba2e
JeffBuss <turbocapslock@gmail.com>
2017-02-26 23:30:13 -0700

    Remove unnecessary files/packages

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import './styles.scss';
+
 import App from './components/App';
 
 const main = document.getElementById('main');
